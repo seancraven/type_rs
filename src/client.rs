a962800611eb5ce187ef893e3384d4b999dc14c9
seancraven <seanpietercraven@gmail.com>
2023-05-04 16:25:04 +0100

    further updates

--- src/client.rs
+++ src/client.rs
@@ -4,12 +4,15 @@ use std::{
 };
 
 #[derive(Debug, Clone)]
-struct IPV4 {
+pub struct IPV4 {
     address: String,
 }
 impl IPV4 {
-    fn address(&self, port: u16) -> String {
+    fn address(&self, port: String) -> String {
         format!("{}:{}", self.address, port)
+    }
+    fn new(address: String) -> IPV4 {
+        IPV4 { address }
     }
 }
 pub struct Client {
@@ -17,26 +20,36 @@ pub struct Client {
     port: String,
 }
 impl Client {
-    pub fn new(host_ip: IPV4, port: u16) -> Client {
+    pub fn new(host_ip: IPV4, port: String) -> Client {
         Client { host_ip, port }
     }
     pub fn address(&self) -> String {
-        self.host_ip.address(self.port)
+        self.host_ip.address(self.port.clone())
     }
     /// Client starts, forms a connection and then returns an iterator over the response.
-    pub fn start_gen(&self) -> Result<TcpListener, io::Error> {
+    pub fn start_gen(&self) -> Result<(), io::Error> {
         let address = self.address();
+        println!("Connecting to {}", address);
         let mut stream = TcpStream::connect(&address)?;
         stream.write_all("start".as_bytes())?;
-        let listner = TcpListener::bind(&address)?;
-        return Ok(listner);
+        Ok(())
     }
 }
 
 #[cfg(test)]
 mod client_test {
+    use super::*;
+    use dotenv::dotenv;
+    use std::{
+        env::var,
+        io::{BufRead, BufReader},
+    };
     #[test]
     fn server_test() {
-        todo!()
+        dotenv().ok();
+        let ip = IPV4::new(var("IPV4").expect("Can't find .evn variable IPV4"));
+        let port = var("PORT").expect("Can't find .evn variable IPV4");
+        let client = Client::new(ip, port);
+        let listner = client.start_gen().expect("Failed to connect to host");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,4 @@ use client::Client;
 use dotenv::dotenv;
 use std::env::var;
 
-fn main() {
-    dotenv().ok();
-    let ip = var("IPV4").expect("Can't find .evn variable IPV4");
-    let port = var("PORT").expect("Can't find .evn variable IPV4");
-    let client = Client::new(ip, port).expect("Failed to connect to host");
-}
+fn main() {}
